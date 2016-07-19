7ad63b95364fdd89c90d22dd19c6b4f84bbe3df9
Michael <michael@websr.eu>
2016-07-19 13:49:21 +0200

    Create Config

--- rbd-volume/entrypoint.sh
+++ rbd-volume/entrypoint.sh
@@ -57,6 +57,18 @@ if [[ ! -e /etc/ceph/${CLUSTER}.conf ]]; then
 fi
 }
 
+function get_admin_key {
+   kviator --kvstore=${KV_TYPE} --client=${KV_IP}:${KV_PORT} ${KV_TLS} get ${CLUSTER_PATH}/adminKeyring > /etc/ceph/${CLUSTER}.client.admin.keyring
+}
+
+# ceph admin key exists or die
+function check_admin_key {
+if [[ ! -e /etc/ceph/${CLUSTER}.client.admin.keyring ]]; then
+    echo "ERROR- /etc/ceph/${CLUSTER}.client.admin.keyring must exist; get it from your existing mon"
+    exit 1
+fi
+}
+
 # Map the rbd volume
 function map {
 	/usr/bin/rbd map ${RBD_IMAGE} --pool ${RBD_POOL} -o ${RBD_OPTS}
@@ -78,8 +90,10 @@ function unmap {
 	/usr/bin/rbd unmap $( /usr/bin/rbd showmapped | grep -m 1 -E "^[0-9]{1,3}\s+${RBD_POOL}\s+${RBD_IMAGE}" | awk '{print $5}' )
 }
 
-get_config &&
-check_config &&
+get_admin_key
+check_admin_key
+get_config
+check_config
 
 case "$@" in
 	"map" ) map;;
